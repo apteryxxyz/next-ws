a488969abcca69d0af7f60157d0a7f766f7f6993
Apteryx <apteryx.xyz@gmail.com>
2023-07-30 04:32:07 +1200

    Bump `patch-2` supported range

--- packages/cli/src/patches/patch-2.ts
+++ packages/cli/src/patches/patch-2.ts
@@ -14,7 +14,7 @@ const NextTypesFilePath = path.join(
 const NextTypesPatch = 'SOCKET?: Function';
 
 // Add `SOCKET?: Function` to the page module interface check field thing in
-// `next/dist/build/webpack/plugins/next-types-plugin.js`
+// `next/dist/build/webpack/plugins/next-types-plugin/index.js`
 export function patchNextTypesPlugin() {
     log.info("Adding 'SOCKET' to the page module interface type...");
     const content = fs.readFileSync(NextTypesFilePath, 'utf8');
@@ -33,9 +33,8 @@ export default Object.assign(
         patchNextTypesPlugin();
     },
     {
-        // NOTE: As 15/07/23 the latest version of Next.js is 13.4.10
         date: '2023-07-15' as const,
         // The file for 'next-types-plugin' was moved in 13.4.9
-        supported: '>=13.4.9 <=13.4.10' as const,
+        supported: '>=13.4.9 <=13.4.12' as const,
     }
 );
