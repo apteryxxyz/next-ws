ea565aeb323874d6d1dcc803eee5308f01765cc3
Kodie <apteryx.xyz@gmail.com>
2024-10-28 14:46:02 +1300

    Update tooling and workflows (#44)
    * Update tooling and workflows
    
    * Fix pr label glob strings
    
    * Bump labeller version
    
    * Change dependencies files glob

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,5 +1,15 @@
-import { defineConfig } from '@configs/tsup';
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/*/index.ts'],
+  format: ['esm', 'cjs'],
+  target: 'es2022',
+  clean: true,
+  splitting: true,
+  bundle: true,
+  treeshake: true,
+  keepNames: true,
+  minifySyntax: true,
+  dts: true,
+  sourcemap: true,
 });
