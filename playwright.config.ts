3f60442313ee034621085e7403ad42b609f61d8e
Apteryx <apteryx.xyz@gmail.com>
2025-02-06 21:26:45 +1300

    Add playwright traces

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,17 +4,21 @@ export default defineConfig({
   testDir: 'tests',
   reporter: [['html', { outputFolder: 'tests/.report' }]],
   outputDir: 'tests/.results',
+  retries: 1,
+  use: { trace: 'on-first-retry' },
   webServer: [
     {
       cwd: 'examples/chat-room',
       command: 'pnpm dev --port 3001',
       port: 3001,
+      stdout: 'pipe',
       reuseExistingServer: !process.env.CI,
     },
     {
       cwd: 'examples/chat-room-with-custom-server',
       command: 'pnpm dev --port 3002',
       port: 3002,
+      stdout: 'pipe',
       reuseExistingServer: !process.env.CI,
     },
   ],
