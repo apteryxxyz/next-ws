e318e37f2eedfb2a15d22baff14fc50323dab213
Kodie <apteryx.xyz@gmail.com>
2025-08-27 17:53:32 +1200

    Bump patch supported range to 15.5.2 (#119)
    Co-authored-by: apteryxxyz <apteryxxyz@users.noreply.github.com>

--- src/patches/patch-2.ts
+++ src/patches/patch-2.ts
@@ -20,7 +20,7 @@ export const patchCookies = definePatchStep({
 
 export default definePatch({
   name: 'patch-2',
-  versions: '>=15.0.0 <=15.5.1',
+  versions: '>=15.0.0 <=15.5.2',
   steps: [
     p1_patchNextNodeServer,
     p1_patchRouterServer,
